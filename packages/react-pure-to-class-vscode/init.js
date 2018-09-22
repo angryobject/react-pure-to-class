cc2b589490d9d5f7274e2a3912dc6d5ea750573f
Max Shishkin <angryobject@users.noreply.github.com>
2018-09-22 18:25:01 +0300

    Class to pure with react-codemod

--- packages/react-pure-to-class-vscode/init.js
+++ packages/react-pure-to-class-vscode/init.js
@@ -2,65 +2,80 @@
 
 const jscodeshift = require('jscodeshift');
 const pureToClass = require('react-pure-to-class');
+const classToPure = require('react-codemod/transforms/pure-component');
 
 module.exports = vscode => ({
   activate(context) {
-    const disposable = vscode.commands.registerCommand(
-      'extension.reactPureToClass',
-      () => {
-        const editor = vscode.window.activeTextEditor;
-        const config = vscode.workspace.getConfiguration('reactPureToClass');
-        const supportedLangs = ['javascript', 'javascriptreact'].concat(
-          config.typescript ? ['typescript', 'typescriptreact'] : []
-        );
+    const config = vscode.workspace.getConfiguration('reactPureToClass');
 
-        if (!editor) {
-          return;
-        }
-
-        const doc = editor.document;
-
-        if (supportedLangs.indexOf(doc.languageId) === -1) {
-          const msg = config.typescript
-            ? 'Only available for javascript/typescript/react file types'
-            : 'Only available for javascript/react file types. Check settings for typescript support';
-
-          vscode.window.showInformationMessage(msg);
-          return;
-        }
-
-        const selection = editor.selection;
-        const text = doc.getText(selection);
-
-        let output;
-
-        try {
-          output = pureToClass(
-            { source: text },
-            {
-              jscodeshift,
-              stats: () => {},
-            },
-            { reactComponent: config.reactComponent }
-          );
-        } catch (e) {
-          vscode.window.showInformationMessage(
-            'Something went wrong (probably bad selection)'
-          );
-          return;
-        }
-
-        if (output === text) {
-          vscode.window.showInformationMessage('Nothing to transform');
-          return;
-        }
-
-        editor.edit(function(builder) {
-          builder.replace(selection, output);
-        });
-      }
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        'extension.reactPureToClass',
+        transform.bind(null, vscode, pureToClass)
+      )
     );
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        'extension.reactClassToPure',
+        transform.bind(null, vscode, classToPure, {
+          useArrows: config.classToPureUseArrows,
+          destructuring: config.classToPureDestructuring,
+        })
+      )
+    );
   },
 });
+
+function transform(vscode, fn, opts) {
+  const editor = vscode.window.activeTextEditor;
+  const config = vscode.workspace.getConfiguration('reactPureToClass');
+  const supportedLangs = ['javascript', 'javascriptreact'].concat(
+    config.typescript ? ['typescript', 'typescriptreact'] : []
+  );
+
+  if (!editor) {
+    return;
+  }
+
+  const doc = editor.document;
+
+  if (supportedLangs.indexOf(doc.languageId) === -1) {
+    const msg = config.typescript
+      ? 'Only available for javascript/typescript/react file types'
+      : 'Only available for javascript/react file types. Check settings for typescript support';
+
+    vscode.window.showInformationMessage(msg);
+    return;
+  }
+
+  const selection = editor.selection;
+  const text = doc.getText(selection);
+
+  let output;
+
+  try {
+    output = fn(
+      { source: text },
+      {
+        jscodeshift,
+        stats: () => {},
+      },
+      Object.assign({ reactComponent: config.reactComponent }, opts)
+    );
+  } catch (e) {
+    vscode.window.showInformationMessage(
+      'Something went wrong (probably bad selection)'
+    );
+    return;
+  }
+
+  if (!output || output === text) {
+    vscode.window.showInformationMessage('Nothing to transform');
+    return;
+  }
+
+  editor.edit(function(builder) {
+    builder.replace(selection, output);
+  });
+}
